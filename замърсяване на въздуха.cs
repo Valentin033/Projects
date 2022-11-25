989ec479fabd7a92181455353fa5d69b73a98e43
Valentin Solakov <119061115+Valentin033@users.noreply.github.com>
2022-11-25 17:31:29 +0200

    Add files via upload
    Проектът е недовършен. Предназначен е да взима информация от интернет и да се ъпдейтва за замърсяване на въздуха в София.

---
+++ замърсяване на въздуха.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebRequest request = WebRequest.Create(https://maps.sensor.community/ );
+
+                request.Credentials = (CredentialCache).DefaultCredentials);
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            Console.WriteLine(response.StatusDescription);
+
+            Stream dataStream = response.GetResponseStream();
+
+            StreamReader reader = new StreamReader(dataStream);
+
+            string responseFromServer = reader.ReadToEnd();
+
+            Console.WriteLine(responseFromServer);
+
+            reader.Close();
+            dataStream.Close();
+            response.Close();
+
+
+
+        }
+    }
+}
